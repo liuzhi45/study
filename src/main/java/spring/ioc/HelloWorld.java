a22be42636697b6b461e6ec945b3df1e0d5614a0
liuzhi <13718624701@163.com>
2017-08-25 12:24:27 +0800

    test

--- src/main/java/spring/ioc/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package spring.ioc;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.err.println("helllo world");
-	}
-}
---
+++ src/main/java/spring/ioc/HelloWorldBean.java
@@ -0,0 +1,10 @@
+package spring.ioc;
+
+
+public class HelloWorldBean {
+	
+	public String sayHello(){
+		
+		return "Hello World";
+	} 
+}
